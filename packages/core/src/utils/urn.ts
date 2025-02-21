ec526018a73256d6152c1bd7f65108e608771253
aeolian <94939382+aeolianeth@users.noreply.github.com>
2025-02-21 14:31:50 +1000

    fartcoin

---
+++ packages/core/src/utils/urn.ts
@@ -0,0 +1,26 @@
+import { JB_CHAIN_SLUGS } from "../constants.js";
+import { JBChainId } from "../types.js";
+
+export function jbUrn(urn: string): {
+  chainId: JBChainId;
+  projectId: bigint;
+} {
+  const [chainSlug, projectId] = urn.split(":");
+  const chain = JB_CHAIN_SLUGS[chainSlug];
+
+  return {
+    chainId: chain.chain.id as JBChainId,
+    projectId: BigInt(projectId),
+  };
+}
+
+export function toJbUrn(chainId: JBChainId, projectId: bigint): string | null {
+  const chain = Object.values(JB_CHAIN_SLUGS).find(
+    (chain) => chain.chain.id === chainId
+  );
+  if (!chain) {
+    return null;
+  }
+
+  return `${chain.slug}:${projectId}`;
+}
