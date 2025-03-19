90bd59cc32d7a256991451c385f2c12c312310ee
aeolian <94939382+aeolianeth@users.noreply.github.com>
2025-03-20 08:29:05 +1000

    relayr url update

--- packages/react/src/lib/relayr/constants.ts
+++ packages/react/src/lib/relayr/constants.ts
@@ -1,2 +1,2 @@
-export const API = "https://relayr-api-staging.up.railway.app";
-export const DASHBOARD = "https://relayr-dashboard-staging.up.railway.app";
+export const API = "https://api.relayr.ba5ed.com/";
+export const DASHBOARD = "https://relayr.ba5ed.com/";
