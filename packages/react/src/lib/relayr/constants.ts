0e1041645a0c371d2233eed9c807cc36a09d4246
aeolian <94939382+aeolianeth@users.noreply.github.com>
2025-03-23 13:54:17 +1000

    rev

--- packages/react/src/lib/relayr/constants.ts
+++ packages/react/src/lib/relayr/constants.ts
@@ -1,2 +1,2 @@
-export const API = "https://api.relayr.ba5ed.com/";
-export const DASHBOARD = "https://relayr.ba5ed.com/";
+export const API = "https://api.relayr.ba5ed.com";
+export const DASHBOARD = "https://relayr.ba5ed.com";
