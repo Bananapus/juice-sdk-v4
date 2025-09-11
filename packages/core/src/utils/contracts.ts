0703461e93516b27050b965b9e97262b6ef7a3b4
Riderway <riderway@runbox.com>
2025-09-11 19:57:04 +0200

    Fix primaryTerminal address for projects with USDC context

--- packages/core/src/utils/contracts.ts
+++ packages/core/src/utils/contracts.ts
@@ -1,21 +1,33 @@
-import { NATIVE_TOKEN } from "../constants.js";
+import { Address, zeroAddress } from "viem";
+import { NATIVE_TOKEN, USDC_ADDRESSES } from "../constants.js";
 import { readJbDirectoryPrimaryTerminalOf } from "../generated/juicebox.js";
 import { JBChainId } from "../types.js";
-import { Address } from "viem";
 
 export async function getProjectTerminalStore(
   config: any, // TODO wagmi config
   chainId: JBChainId,
   projectId: bigint
 ) {
+  const terminal = await getPrimaryNativeTerminal(config, chainId, projectId);
+
+  const terminalStoreData = await fetch(
+    `https://juicebox.money/api/juicebox/v4/terminal/${terminal}/jb-terminal-store?chainId=${chainId}`
+  ).then((res) => res.json());
+
+  const terminalStore = terminalStoreData.terminalStoreAddress as Address;
+  return terminalStore;
+}
+
+export async function getPrimaryNativeTerminal(config: any, chainId: JBChainId, projectId: bigint) {
   const primaryNativeTerminal = await readJbDirectoryPrimaryTerminalOf(config, {
-    chainId: Number(chainId) as JBChainId,
+    chainId,
     args: [projectId, NATIVE_TOKEN],
   });
-  const terminalStoreData = await fetch(
-    `https://juicebox.money/api/juicebox/v4/terminal/${primaryNativeTerminal}/jb-terminal-store?chainId=${chainId}`
-  ).then((res) => res.json());
-  const terminalStore = terminalStoreData.terminalStoreAddress as Address;
 
-  return terminalStore;
+  if (primaryNativeTerminal !== zeroAddress) return primaryNativeTerminal;
+
+  return await readJbDirectoryPrimaryTerminalOf(config, {
+    chainId,
+    args: [projectId, USDC_ADDRESSES[chainId]],
+  });
 }
